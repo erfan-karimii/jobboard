9792c9a0f8cda0ed3ef83b3dc5236775e0aa78ef
erfan-karimi <h410mi3@gmail.com>
2024-08-07 19:51:38 +0330

    add smtp4dev server to dev and prod

--- core/core/settings/prod.py
+++ core/core/settings/prod.py
@@ -8,3 +8,11 @@ CSRF_TRUSTED_ORIGINS = config(
     cast=lambda v: [s.strip() for s in v.split(",")],
     default="http://*",
 )
+
+ADMIN_EMAIL = config("ADMIN_EMAIL",default="admin@admin.com")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = False
+EMAIL_HOST = config("EMAIL_HOST",default="smtp4dev")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER",default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD",default="")
+EMAIL_PORT = 25
