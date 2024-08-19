51164f7ae0e72d3480bec6f1c14d168e31a54aec
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2024-08-19 16:50:29 +0330

    make urls more clean

--- core/account/urls.py
+++ core/account/urls.py
@@ -1,14 +1,21 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
 
 
 app_name = "account" 
 
-urlpatterns = [
+user_urlpatterns = [
     path('login/',views.CustomerLoginView.as_view(),name='login-view'),
-    # path('companylogin/',views.CompanyLogin.as_view(),name='company-login'),
     path('flog/',views.login_view,name='flog'),
     path('profile/',views.CustomerProfile.as_view(),name='profile'),
-    path('company/login/',views.CompanyLoginView.as_view(),name='company-login'),
-    
+]
+
+company_urlpatterns = [
+    path('login/',views.CompanyLoginView.as_view(),name='company-login'),
+]
+
+
+urlpatterns = [
+    path('user/',include(user_urlpatterns)),
+    path('company/',include(company_urlpatterns)),
 ]
