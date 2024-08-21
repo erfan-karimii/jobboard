3ad9b6e5188552a1cf9d1b62c162501a3007bde5
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-08-21 20:46:33 +0330

    Show Job

--- core/job/views.py
+++ core/job/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Job
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+
+class ShowJobs(APIView):
+
+    class ShowJobSerializers(serializers.ModelSerializer):
+        url = serializers.HyperlinkedIdentityField(view_name='job:job-detail', read_only=True)
+        class Meta:
+            model= Job
+            fields = ['title','company','province','url']
+
+    def get(self,request):
+        jobs=Job.objects.filter(status=True)
+        serializer=self.ShowJobSerializers(jobs,many=True,context={'request':request})
+
+        return Response(serializer.data)
+
+
+class ShowDetailJob(APIView):
+    class ShowDetailJobSerializer(serializers.ModelSerializer):
+        # url = serializers.HyperlinkedIdentityField(view_name='job-detail', read_only=True)
+        class Meta:
+            model= Job
+            fields ="__all__"
+
+    def get(self,request,pk):
+        job = get_object_or_404(Job,id=pk,status=True)
+        ser = self.ShowDetailJobSerializer(job)
+        return Response(ser.data)
+        
