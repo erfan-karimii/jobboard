0872d3bbb437e1261684cf4fa597b6348cff086f
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-09-03 12:58:20 +0330

    update django version

--- core/account/permissions.py
+++ core/account/permissions.py
@@ -25,26 +25,3 @@ class IsAuthenticatedCompany(permissions.BasePermission):
         user = request.user
         return bool(user and user.is_authenticated and (user.role.role == "company"))
 
-
-# class IsSuperuser(permissions.BasePermission):
-#     """
-#     permission check for if user is valid superuser or not.
-#     """
-
-#     message = "You are not a valid super user!"
-
-#     def has_permission(self, request, view):
-#         user = request.user
-#         return bool(user and user.is_authenticated and user.is_superuser)
-
-
-# class IsDriver(permissions.BasePermission):
-#     """
-#     permission check for if user is valid Driver or not.
-#     """
-
-#     message = "You are not a valid Driver!"
-
-#     def has_permission(self, request, view):
-#         user = request.user
-#         return bool(user and user.is_authenticated and user.is_driver)
--- core/core/settings/base.py
+++ core/core/settings/base.py
@@ -74,6 +74,9 @@ DATABASES = {
         'PASSWORD': config("POSTGRES_PASSWORD", default="admin"),
         'HOST': 'db',
         'PORT': 5432,
+        "OPTIONS": {
+            "pool": True,
+        },
     }
 }
 
